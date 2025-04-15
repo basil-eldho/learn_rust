669995f35d078dcad8adebc22d8e0cbef868355e
Basil Eldho <basileljol@gmail.com>
2025-04-14 23:55:16 -0400

    implement bubble-sorting in rust

--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -3,3 +3,15 @@ fn main() {
     let input = [5, 8, 95, 6, 844, 1, 2, 5, 56];
     println!("Hello, world!");
 }
+
+// Bubble Sort in Rust (in-place, generic)
+fn bubble_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+    for i in 0..len {
+        for j in 0..len - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
