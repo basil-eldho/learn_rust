7f6d4e4367142d7e4dc4075b86d37ad79554791d
Basil Eldho <basileljol@gmail.com>
2025-04-14 23:55:32 -0400

    implement selection-sorting in rust

--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -15,3 +15,19 @@ fn bubble_sort<T: Ord>(arr: &mut [T]) {
         }
     }
 }
+
+// Selection Sort in Rust (in-place, generic)
+fn selection_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+    for i in 0..len {
+        let mut min_idx = i;
+        for j in (i + 1)..len {
+            if arr[j] < arr[min_idx] {
+                min_idx = j;
+            }
+        }
+        if min_idx != i {
+            arr.swap(i, min_idx);
+        }
+    }
+}
