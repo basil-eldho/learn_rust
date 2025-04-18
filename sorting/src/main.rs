c2b8cdbc2c14d645ec091246c8607f3d05983462
Basil Eldho <basileljol@gmail.com>
2025-04-18 00:08:59 -0400

    add merge sort

--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -8,14 +8,16 @@ fn main() {
     }
 
     let mut rng = rand::rng();
-    let mut numbers: Vec<i32> = (0..10).map(|_| rng.random_range(0..100)).collect();
+    let mut numbers: Vec<i32> = (0..7).map(|_| rng.random_range(0..100)).collect();
+    let len = numbers.len();
     println!("{:?}", numbers);
 
     match sort_alg.as_str() {
         "bubble-sort" => bubble_sort(&mut numbers),
         "selection-sort" => selection_sort(&mut numbers),
         "insertion-sort" => insertion_sort(&mut numbers),
-        _ => selection_sort(&mut numbers),
+        "merge-sort" => merge_sort(&mut numbers),
+        _ => merge_sort(&mut numbers),
     }
     println!("{:?}", numbers);
 }
@@ -51,16 +53,48 @@ fn selection_sort<T: PartialOrd>(arr: &mut [T]) {
 
 fn insertion_sort<T: PartialOrd>(arr: &mut [T]) {
     for i in 1..arr.len() {
-        let mut j = i;
-        while j > 0 && arr[j] < arr[j - 1] {
-            arr.swap(j, j - 1);
-            j -= 1;
+        for j in 1..=i {
+            if arr[j] < arr[j - 1] {
+                arr.swap(j, j - 1);
+            }
         }
+    }
+}
 
-        // for j in (1..=i) {
-        //     if arr[j] < arr[j-1] {
-        //         arr.swap(j, j-1);
-        //     }
-        // }
+fn merge_sort<T: PartialOrd + Clone>(arr: &mut [T]) {
+    let len = arr.len();
+    if len <= 1 {
+        return;
+    }
+
+    let mid = len / 2;
+    let mut left = arr[..mid].to_vec();
+    let mut right = arr[mid..].to_vec();
+    merge_sort(&mut left);
+    merge_sort(&mut right);
+    merge(&left, &right, arr)
+}
+
+fn merge<T: PartialOrd + Clone>(left: &[T], right: &[T], arr: &mut [T]) {
+    let (mut i, mut j) = (0, 0);
+
+    while i < left.len() && j < right.len() {
+        if left[i] <= right[j] {
+            arr[i + j] = left[i].clone();
+            i += 1;
+        } else {
+            arr[i + j] = right[j].clone();
+            j += 1;
+        }
+    }
+
+    while i < left.len() {
+        arr[i + j] = left[i].clone();
+        i += 1;
+    }
+
+    while j < right.len() {
+        arr[i + j] = right[j].clone();
+        j += 1;
     }
 }
